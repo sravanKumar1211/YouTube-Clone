5cc1c18c6dfcef157e5eb70145c0eafaf728a928
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-11-28 14:51:26 +0530

    horizontal scroll bar with all categories are created in home page

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+
+import React from "react";
 
 function HomePage() {
+  const categories = ["All","Software","News","Sports","Gaming","Tollywood","Bollywood","Music","Web Development","DSA","AI & ML","Stocks",
+    "Podcasts","Movies","History","Fitness","Technology","Trending","Live"];
+
   return (
-   <>
-   </>
-  )
+    <>
+      <div className="ml-60 px-4 py-2 mt-[-40px]">
+
+        {/* Horizontal Scroll Category Bar */}
+        <div className="flex overflow-x-auto space-x-3 py-2">
+
+          {categories.map((cat, index) => (
+            <div
+              key={index}
+              className="px-4 py-2 bg-gray-100 rounded-xl whitespace-nowrap text-sm cursor-pointer hover:bg-gray-200"
+            >
+              {cat}
+            </div>
+          ))}
+
+        </div>
+
+      </div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
