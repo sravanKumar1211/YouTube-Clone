154d5eb62443a2b3b6ef61f361ccb26a55607458
sravanKumar1211 <sravankumargaddamedhi@gmail.com>
2025-12-06 01:29:25 +0530

    backed connectd to live server

---
+++ src/api/apiClient.js
@@ -0,0 +1,8 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "https://youtube-backend-1-ury4.onrender.com",  // your backend
+//    withCredentials: true,
+// });
+
+// export default API;
